7a68dcfcf9fad7cbef695f7c4ffb65f7f115807b
cepheidxa <zbsdta@126.com>
2017-03-11 20:41:52 +0800

    change error at cppcheck and valgrind

--- delayed_module_check.c
+++ delayed_module_check.c
@@ -13,15 +13,14 @@ int start_delayed_module_init(void)
 	char filename[] = "a.txt";
 	int ret = 0;
 	int fd = open(filename, O_RDWR);
-	if(fd == -1) {
+	if (fd == -1) {
 		error(-1, errno, "%s open failed.", filename);
 		return -1;
 	}
 	lseek(fd, 0, SEEK_SET);
 	ret = write(fd, "1", 1);
-	if(fd == -1) {
+	if (ret == -1) {
 		error(-1, errno, "%s write failed.", filename);
-		return -1;
 	}
 	close(fd);
 	return 0;
@@ -30,38 +29,38 @@ int start_delayed_module_init(void)
 int wait_for_delayed_module_complete(void)
 {
 	char filename[] = "a.txt";
-	char buf[5] = {0,};
+	char buf[5] = { 0, };
 	int ret = 0;
 	int fd = open(filename, O_RDONLY);
 	int retry_count = 100000 * 30;
-	if(fd == -1) {
+	if (fd == -1) {
 		error(-1, errno, "%s open failed.", filename);
 		return -1;
 	}
 	ret = read(fd, buf, 4);
-	if(ret == -1) {
-		error(-1, errno, "%d read failed.", filename);
+	if (ret == -1) {
+		error(-1, errno, "%s read failed.", filename);
 		return -1;
 	}
 	buf[ret] = '\0';
 	close(fd);
-	while(retry_count-- && atoi(buf)) {
+	while (retry_count-- && atoi(buf)) {
 		usleep(10);
 		fd = open(filename, O_RDONLY);
-		if(fd == -1) {
+		if (fd == -1) {
 			error(-1, errno, "%s open failed.", filename);
 			return -1;
 		}
 		ret = read(fd, buf, 4);
-		if(ret == -1) {
-			error(-1, errno, "%d read failed.", filename);
+		if (ret == -1) {
+			error(-1, errno, "%s read failed.", filename);
 			close(fd);
 			return -1;
 		}
 		buf[ret] = '\0';
 		close(fd);
 	}
-	if(retry_count)
+	if (retry_count)
 		return 0;
 	else
 		return -1;
--- sepolicy_data.c
+++ sepolicy_data.c
@@ -10,34 +10,40 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc < 2)
+	int ret;
+	int fd;
+	int size;
+	unsigned char *buf;
+	unsigned char *ch;
+	int i;
+	if (argc < 2)
 		return -1;
-	int fd = open(argv[1], O_RDONLY);
-	if(fd == -1) {
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1) {
 		error(-1, errno, "%s open failed.", argv[1]);
 	}
 
-	int size = lseek(fd, 0, SEEK_END);
-	char *buf = (char *)malloc(size);
-	if(!buf)
+	size = lseek(fd, 0, SEEK_END);
+	buf = (unsigned char *)malloc(size);
+	if (!buf)
 		error(-1, errno, "malloc faild.");
-	int ret = read(fd, buf, size);
 	ret = lseek(fd, 0, SEEK_SET);
-	if(ret == -1)
+	if (ret == -1)
 		error(-1, errno, "%s lseek failed.", argv[1]);
 	ret = read(fd, buf, size);
-	if(ret != size)
+	if (ret != size)
 		error(-1, errno, "%s read failed.", argv[1]);
 	printf("unsigned char zte_sepolicy_data[] = {\n");
-	unsigned char *ch = buf;
-	int i;
-	for(i = 0; i < size - 1; i++) {
+	ch = buf;
+	for (i = 0; i < size - 1; i++) {
 		printf("0x%02x,", *ch);
 		ch++;
-		if((i+1) % 20 == 0)
+		if ((i + 1) % 20 == 0)
 			printf("\n");
 	}
-	printf("0x%02x,", *ch);
+	printf("0x%02x", *ch);
 	printf("};");
+	free(buf);
 	return 0;
 }
