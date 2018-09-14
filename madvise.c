b3d0c72e1665b806a7f5c8e68fc2d681de1d5834
Zhang Bo <zbsdta@126.com>
2018-09-14 22:38:48 +0800

    test memory use

--- madvise.c
+++ madvise.c
@@ -32,17 +32,19 @@ int main(int argc, char *argv[])
 		error(-1, errno, "ftruncate file(%s) failed.", file);
 	}
 
+	//char *addr =
+	//    (char *)mmap(NULL, FILE_SIZE, PROT_WRITE, MAP_SHARED|MAP_LOCKED, fd, 0);
 	char *addr =
-	    (char *)mmap(NULL, FILE_SIZE, PROT_WRITE, MAP_SHARED|MAP_LOCKED, fd, 0);
+	    (char *)mmap(NULL, FILE_SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
 	if (addr == MAP_FAILED) {
 		error(-1, errno, "mmap file(%s) failed.", file);
 	}
 
-	//ret = madvise(addr, FILE_SIZE, MADV_DONTNEED);
+	ret = madvise(addr, FILE_SIZE, MADV_DONTNEED);
 	//ret = madvise(addr, FILE_SIZE, MADV_WILLNEED);
-	//if(ret == -1) {
-	//	error(-1, errno, "madvise failed.");
-	//}
+	if(ret == -1) {
+		error(-1, errno, "madvise failed.");
+	}
 
 	//ret = munlock(addr, FILE_SIZE + 10000);
 	//if(ret == -1) {
@@ -52,10 +54,41 @@ int main(int argc, char *argv[])
 	printf("addr = %pF\n", addr);
 
 	//usleep(1000000000);
+	long page_size = sysconf(_SC_PAGESIZE);
+	int vec_len = (FILE_SIZE+page_size-1) / page_size;
+	unsigned char *p = (unsigned char *)malloc(vec_len);
+	if(p == NULL) {
+		error(-1, errno, "malloc failed.");
+	}
+	memset(p, 0, vec_len);
+	ret = mincore(addr, FILE_SIZE, p);
+	if(ret == -1) {
+		error(-1, errno, "mincore failed.");
+	}
+	for(int i = 0; i < vec_len; i++)
+		printf("%x", p[i]);
+	printf("\n");
+	free(p);
 
 	int i;
-	for(i = 0; i < FILE_SIZE; i++) {\
-		//usleep(1);
+	for(i = 0; i < FILE_SIZE; i+=page_size) {\
+		usleep(1000000);
+		if(1) {
+			int vec_len = (FILE_SIZE+page_size-1) / page_size;
+			unsigned char *p = (unsigned char *)malloc(vec_len);
+			if(p == NULL) {
+				error(-1, errno, "malloc failed.");
+			}
+			memset(p, 0, vec_len);
+			ret = mincore(addr, FILE_SIZE, p);
+			if(ret == -1) {
+				error(-1, errno, "mincore failed.");
+			}
+			for(int i = 0; i < vec_len; i++)
+				printf("%x", p[i]);
+			printf("\n");
+			free(p);
+		}
 		addr[i] = i%255;
 	}
 	//ret = madvise(addr, FILE_SIZE, MADV_DONTNEED);
